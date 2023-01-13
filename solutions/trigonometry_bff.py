95f0087f4ac719f92b1c0bd7c920e64f40e7503e
Kittenlover229 <artur.roos@mail.ru>
2023-01-13 12:20:14 +0200

    Fix an unfinisehd part of the exercise

--- solutions/trigonometry_bff.py
+++ solutions/trigonometry_bff.py
@@ -1,9 +1,11 @@
 import turtle
 from math import sin, pi
-t = turtle
 
 a = int(input())
 b = int(input())
 
 for i in range(0, 200 + 1):
-    t.goto(i, a * sin(pi * i / 10) + b)
+    turtle.goto(i, a * sin(pi * i / 10) + b)
+print(turtle.pos())
+
+turtle.exitonclick()
